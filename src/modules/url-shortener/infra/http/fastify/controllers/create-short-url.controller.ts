96d16de373a0a1f44996680dab1a5f0fa0940f5f
Rian Marlon <rianmarlon20@yahoo.com>
2025-05-03 19:53:57 -0300

    feat: add create short url controller

---
+++ src/modules/url-shortener/infra/http/fastify/controllers/create-short-url.controller.ts
@@ -0,0 +1,22 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { container } from 'tsyringe';
+
+import { CreateShortUrlUseCase } from '@/modules/url-shortener/application/usecases/create-short-url/create-short-url.usecase';
+
+interface CreateShortUrlRequest {
+  originalUrl: string;
+}
+
+export class CreateShortUrlController {
+  async handle(request: FastifyRequest, reply: FastifyReply): Promise<FastifyReply> {
+    const { originalUrl } = request.body as CreateShortUrlRequest;
+
+    const createShortUrlUseCase = container.resolve(CreateShortUrlUseCase);
+
+    const result = await createShortUrlUseCase.execute({
+      originalUrl,
+    });
+
+    return reply.status(201).send(result);
+  }
+}
